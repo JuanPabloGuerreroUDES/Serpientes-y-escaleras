d99a8783638923d894884537a2c933e0b3175a12
Juan Pablo Guerrero <101900989+JuanPabloGuerreroUDES@users.noreply.github.com>
2024-05-15 17:08:33 -0500

    Add files via upload

---
+++ SerpientesYEscaleras/src/PAQUETE/Juego.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PAQUETE;
+
+import java.util.Random;
+
+/**
+ *
+ * @author USER
+ */
+public class Juego {
+    private int dado;
+    private int turno=0;
+
+    
+    public Juego() {
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    
+    
+    
+    
+    public void lanzarDado(){
+        Random random =new Random();
+        this.dado=random.nextInt(6)+1;
+    }
+    
+}
