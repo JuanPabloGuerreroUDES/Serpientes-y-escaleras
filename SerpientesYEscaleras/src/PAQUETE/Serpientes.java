02504e18b4a53acbe1c163f377ca13c2f69b8b36
Juan Pablo Guerrero <101900989+JuanPabloGuerreroUDES@users.noreply.github.com>
2024-05-17 00:42:06 -0500

    Add files via upload

---
+++ SerpientesYEscaleras/src/PAQUETE/Serpientes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PAQUETE;
+
+/**
+ *
+ * @author juang
+ */
+public class Serpientes {
+    private int inicio;
+    private int fin;
+
+    public Serpientes() {
+    }
+    
+    
+
+    public Serpientes(int inicio, int fin) {
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public void setFin(int fin) {
+        this.fin = fin;
+    }
+}
